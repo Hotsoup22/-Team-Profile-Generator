c7b08f2466e88b68a5ec91389af5119059058920
hotsoup42 <jose22gw@gmail.com>
2021-08-26 14:05:11 -0700

    cli is working great and logging the correct user info.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// const FileIO = require("./fileIO");
+const Employee = require("./lib/Employee");
+ const Manager = require("./lib/Manager");
+ const Engineer = require("./lib/Engineer");
+ const Intern = require("./lib/Intern");
+  let teamMembers = [];
+ 
+ const generateHtml = require('./src/indexTemplate');
 
 // const fileIO = new FileIO();
 
-
-
 // // TODO: Include packages needed for this application
 
-
 const inquirer = require("inquirer");
-const employee = [""] ;
+
 // TODO: Create an array of questions for user input
 const managerQuestions = [
   {
@@ -17,217 +20,158 @@ const managerQuestions = [
     name: "name",
   },
   {
-    type:"input",
+    type: "input",
     message: "what is the team manager's id?",
-     name: "id"
+    name: "id",
   },
   {
-    type:"input",
-    message:"What is the team Manager's Email Adress?",
-    name:"email",
+    type: "input",
+    message: "What is the team Manager's Email Adress?",
+    name: "email",
   },
   {
-    type:"input",
-    message:"What is the team Manager's office Number?",
-    name: "office number"
+    type: "input",
+    message: "What is the team Manager's office Number?",
+    name: "officeNum",
   },
 ];
-  const addMemberQ = [
+const addMemberQ = [
   {
     type: "list",
     name: "addMember",
     message: "Which type of team member would you like to add?",
-    choices:["Engineer", "Intern", "employee", "no more new peeps"],
+    choices: ["Engineer", "Intern", "no more new peeps", "Manager"],
   },
 ];
 const engineerQuestions = [
   {
-    type:"input",
+    type: "input",
     message: "What is the team's new Engineer's name? ",
-    name:"name",
+    name: "name",
   },
   {
-    type:"input",
-    message:"What is the team Engineer's Id?",
-    name:"id",
+    type: "input",
+    message: "What is the team Engineer's Id?",
+    name: "id",
   },
   {
-    type:"input",
-    message:"What is the team Engineer's Email Address?",
-    name:"email"
+    type: "input",
+    message: "What is the team Engineer's Email Address?",
+    name: "email",
   },
   {
-    type:"input",
-    message:"What is your Engineer's Github username?",
-    name:"username"
-  }
-]
-  const internQuestions = [
-    {
-      type: "input",
-      message:"What is the team's new Intern's name?",
-      name: "name",
-    },
-    {
-      type:"input",
-      message:"What is your Intern's id?",
-      name: "id",
-    },
-    {
-      type:"input",
-      message:"what is your Intern's Email Adress?",
-      name: "email",
-    },
-      {
-        type:'input',
-        message:"What is your intern's school?",
-        name:"school",
-      },
+    type: "input",
+    message: "What is your Engineer's Github username?",
+    name: "userName",
+  },
+];
+const internQuestions = [
+  {
+    type: "input",
+    message: "What is the team's new Intern's name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is your Intern's id?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "what is your Intern's Email Adress?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your intern's school?",
+    name: "school",
+  },
+];
 
-   ];
-     //     function writeToFile(userData) {
 
-  //       const fs = require("fs");
-  //       fs.writeFile("output.md", generateMarkdown(userData), (err) =>
-  //         err ? console.error(err) : console.log("Success!")
-  //       );
-  //     }
-
-    const promptAddMember = () =>{
-     inquirer.prompt(addMemberQ)
-    .then((addMemberAnswer) => {
-     //THEN to use their answers to decide what to do next.
-     addMemberAnswer = addMemberAnswer.addMember
-    //  writeToFile(addMemberAnswer);
-     if(addMemberAnswer === ("Engineer")){
-      return promptEngineerQuestions();
-     }else if(addMemberAnswer === "Intern"){
-      return askForInternInfo();
-     } else console.log("promt add member, else."+ addMemberAnswer+ addMemberQ.name)
-      
-    })
-  }
-  //     const promptEngineerQuestions = () => { 
-  //     return inquirer.prompt(engineerQuestions) = () => inquirer
-  //     .then((engineerAnswers) => {
-  //     const Engineer = new Engineer(engineerAnswers);
-  //     employee.push(Engineer);
-  //     return promptAddMember();
-  //   })
-  // }
-   const promptEngineerQuestions = () =>{
-     inquirer.prompt(engineerQuestions)
-   .then((engineerAnswers) => {
-     engineerAnswers = engineerQuestions.name;
+const promptMenu = () => {
+  writeToFile();
+  inquirer.prompt(addMemberQ).then((addMemberAnswer) => {
     //THEN to use their answers to decide what to do next.
+    addMemberAnswer = addMemberAnswer.addMember;
+    //  writeToFile(addMemberAnswer);
+    if (addMemberAnswer === "Engineer") {
+      //add Engineer to html card. !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      return promptEngineerQuestions();
+    } else if (addMemberAnswer === "Intern") {
+      return askForInternInfo();
+    } else if(addMemberAnswer === "Manager"){
+      return managerQuestions();
+    }else if (addMemberAnswer === "no more new peeps"){
+      return 
+    }else
+      console.log(
+        "promt add member, else." + addMemberAnswer + addMemberQ.addMember
+      );
+  });
+};
 
-   if (engineerAnswers === /^[^a-z][0-9]+$/){
-    const Engineer = new Engineer(engineerAnswers)
-    employee.push(Engineer);
-   return promptAddMember();
-} else {
-  return promptAddMember();
-}
-   //  writeToFile(addMemberAnswer);
-   // if(addMemberAnswer === "Engineer"){
-   //  return
-    //}else if(addMemberAnswer === "Intern"){
-     //return askForInternInfo();
-    //} else console.log("it broke here00")
-     
-   })
- }
+const promptEngineerQuestions = () => {
+  inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
+ 
+    console.log(engineerAnswers)
 
-        //const askForInternInfo = () => {
-        const askForInternInfo = () => inquirer
-          .prompt(internQuestions)
-          .then((internsAnswer) => {
-            internsAnswer = internQuestions.name;
-   
-          if (internsAnswer === /^[^a-z][0-9]+$/){
-            //THEN we need to create a new Intern object with data
-            const Intern = new Intern(internsAnswer)
-            // push the new intern to the list of employees
-             employee.push(Intern);
-             
-           return promptAddMember();
-        } else {
-          return promptAddMember();
-        }
-      })
-        //const askForInternInfo = () => {
-        // const askForInternInfo = () => inquirer
-        //   .prompt(internQuestions)
-        //   .then((internsAnswer) => {
-        //  //THEN to use their answers to decide what to do next.
-        // })
-     
+    teamMembers.push(new Engineer(engineerAnswers));
+      promptMenu();
+
+  });
+};
 
 
-      
-//   `inquirer.prompt()` the user for manager information using our `managerQuestions`
-     
-        inquirer.prompt(managerQuestions)
-          .then((managerAnswers) => {
-           managerAnswers = managerQuestions.name;
-            //then we need to creat a new manager object with th  at data
-             
-             if (managerAnswers === /^[^a-z][0-9]+$/){
-              const Manager = new Manager(managerAnswers);
-             employee.push(Manager);
-             return promptAddMember();
-          }else
-              // AND  THEN new to ask the users what they want to do next. (`inquierer.prompt()` with whatsNextQuestions)
-                return   promptAddMember();
-          })
-          .then(() =>{
-            // User employee objects to create html page and write it to a file
-          })
-          .catch((error) => {
-            if (error.isTtyError) {
-            console.log("manger questions prompt01"+error+"coulndt work bob"+ error.isTtyError)
-            } else {
-              console.log("manger questions prompt01"+error+' Something else went wrong. '+error.isTtyError);
-            }
-          });
+const askForInternInfo = () =>
+  inquirer.prompt(internQuestions).then((internsAnswer) => {
+  console.log(internsAnswer)
 
-
-          
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // TODO: Create a function to write README file
-// function writeToFile(userData) {
-
-//   const fs = require("fs");
-//   fs.writeFile("output.md", generateMarkdown(userData), (err) =>
-//     err ? console.error(err) : console.log("Success!")
-//   );
-// }
-
-// // // TODO: Create a function to initialize app
-// function init() {
-//   inquirer
-//     .prompt(questions)
-
-//     .then((userData) => {
-//       writeToFile(userData);
-//       userData = questions.name;
+    teamMembers.push(new Intern(internsAnswer));
     
-//     });
+    return promptMenu();
+  })
 
-// }
+  
+  function writeToFile() {
 
-// // // // Function call to initialize app
-// init();
+    const fs = require("fs");
+    fs.writeFile('./src/indexTemplate.Html', generateHtml(teamMembers), (err) =>
+      err ? console.error(err) : console.log("Success!")
+    );
+  }
 
+  const promptManagerQuestions = () =>
+  inquirer.prompt(managerQuestions).then((managerAnswers) => {
+   
+    // const name = managerAnswers.name;
+    // const id = managerAnswers.id;
+    // const email = managerAnswers.email;
+    // const officeNum = managerAnswers.officeNum;
+    console.log(managerAnswers);
+    // console.log("id " ,id);
+    // console.log("email " , email);
+    // console.log("officeNum ", officeNum);
+    // managerAnswers = [
+    //   name = managerAnswers.name,
+    //   id = managerAnswers.id,
+    //   email = managerAnswers.email,
+    //   officeNum = managerAnswers.officeNum
+    // ],
+    
+   
+   teamMembers.push(new Manager(managerAnswers));
+
+
+   
+    
+    promptMenu();
+    // AND  THEN new to ask the users what they want to do next. (`inquierer.prompt()` with whatsNextQuestions)
+  })
+
+const init = () =>{
+
+promptManagerQuestions();
+}
+    
+init();
---
+++ src/indexTemplate.js
@@ -0,0 +1,19 @@
+const Manager = require("../lib/Manager");
+
+function generateHtml(Employees) {
+  
+
+
+
+    
+return`hello: ` ;
+;
+
+
+    
+
+  }
+  
+  module.exports = generateHtml;
+
+ 
